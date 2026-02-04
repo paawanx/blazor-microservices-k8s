7968c36dca4bcda9b71236d6bf7b68742878c426
Paawan Srivastava <136116866+paawanx@users.noreply.github.com>
2026-02-05 00:08:38 +0530

    Protect yarp routes

--- BlazorCRUD.ApiGateway/Program.cs
+++ BlazorCRUD.ApiGateway/Program.cs
@@ -1,10 +1,47 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Bearer")
+.AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey =
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    builder.Configuration["Jwt:Key"]!
+                )
+            )
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Authenticated",
+        policy => policy.RequireAuthenticatedUser());
+
+    options.AddPolicy("AllowAnonymous",
+        policy => policy.RequireAssertion(_ => true));
+});
 
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
